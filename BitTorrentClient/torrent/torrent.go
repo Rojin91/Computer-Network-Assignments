916bf83f556e29092175874cf771437843a6fa89
sulavkandel <bkandel.sameer@gmail.com>
2024-07-15 14:44:06 +0545

    updated

--- BitTorrentClient/torrent/torrent.go
+++ BitTorrentClient/torrent/torrent.go
@@ -20,3 +20,22 @@ type InfoDict struct {
 	Name        string `bencode:"name"`
 	Length      int    `bencode:"length"`
 }
+func Open(path string) (*TorrentFile, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	torrent := TorrentFile{}
+	err = bencode.Unmarshal(file, &torrent)
+	if err != nil {
+		return nil, err
+	}
+	return &torrent, nil
+}
+
+func (t *TorrentFile) InfoHash() [20]byte {
+	var buf bytes.Buffer
+	bencode.Marshal(&buf, t.Info)
+	return sha1.Sum(buf.Bytes())
