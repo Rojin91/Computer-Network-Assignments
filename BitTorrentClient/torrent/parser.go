4d5170215718c67ef183d09adca0b6d0c3a35887
Saange <lama8050@gmail.com>
2024-07-23 02:40:28 +0545

    Updated parser and torrent.go

--- BitTorrentClient/torrent/parser.go
+++ BitTorrentClient/torrent/parser.go
@@ -1,11 +1,16 @@
 package torrent
 
-import(
-	"fmt"
-	"os"
+import (
+	"bytes"
+	"crypto/sha1"
+	"github.com/jackpal/bencode-go"
 )
 
-func main(){
-
+//Infohash calculates and return SHA-1 hash of the 'info' dictionary from the .torrent file
+func (t *TorrentFile) InfoHash() [20]byte {
+	var buf bytes.Buffer
+	//Marshall the 'info' dictionary into buffer using bencode
+	bencode.Marshal(&buf, t.Info)
+    //Compute the SHA-1 hash of the buffer's content
+	return sha1.Sum(buf.Bytes())
 }
-
